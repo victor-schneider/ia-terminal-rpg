0a9aa70ec677554336d884e599a33fe58668f306
victor-schneider <victor.8121966@edu.univali.br>
2025-05-01 17:00:11 -0300

    Implement singleton pattern for Gemini model, enhance enemy status display, and add example JSON for player inventory.

--- app/src/main/java/Manegement/ContextCreation.java
+++ app/src/main/java/Manegement/ContextCreation.java
@@ -10,13 +10,14 @@ import com.google.gson.Gson;
 import Components.Context;
 import Repository.ContextRepo;
 import Utils.JsonCleaner;
+import Utils.GeminiSingleton;
 
 import dev.langchain4j.data.message.AiMessage;
 import dev.langchain4j.model.chat.ChatLanguageModel;
 import dev.langchain4j.model.input.Prompt;
 import dev.langchain4j.model.input.PromptTemplate;
 import dev.langchain4j.model.output.Response;
-import dev.langchain4j.model.vertexai.VertexAiGeminiChatModel;
+
 
 public class ContextCreation {
   public static Context main() {
@@ -24,23 +25,36 @@ public class ContextCreation {
     String contexto = "";
     List<String> resposta = new ArrayList<>();
 
-    ChatLanguageModel model = VertexAiGeminiChatModel.builder()
-      .project(System.getenv("PROJECT_ID"))
-      .location(System.getenv("LOCATION"))
-      .modelName("gemini-2.0-flash")
-      .maxOutputTokens(1000)
-      .temperature(2f)
-      .build();
+    ChatLanguageModel model = GeminiSingleton.getInstance();
+
+      // System.out.println("Contexto qeue será passado para a IA: ");
+      // resposta = ContextRepo.listContext();
+      // for(int i = 0; i < resposta.size(); i++) {
+      //   System.out.println(resposta.get(i));
+      // }
     
-      PromptTemplate promptTemplate = PromptTemplate.from(
-        "Você é um Dangeon Master de um jogo de RPG de Dangeons and Dragons com combate por turnos. Seu objetivo aqui é criar a história, ela precisa ser prograssiva, tanto nos monstros que aparecem quanto nos lugares. O jogador sempre irá começar numa vila. Os detalhes de tudo ambientes, construções, clima, horário será criada por você." +
-        "A história será gerada por blocos, você irá criar um contexto de situação. Então, irá fornecer 3 opções de ações. Essas opções devem condizer com o contexto atual da história e as escolhas que o jogador fez anteriormente, caso ele não tenha feito nenhuma escolha, apenas começe a história. Você deve fazer necessáriamente uma opção que levará para um combate, mas não precisa de imediato. Por exemplo, pode-se fazer uma opção que leva o jogador para um dungeon, não terá combate de imediato, mas dentro da dungeon você poderá criar uma opção que levará para um combate." +
-        "Quando o jogador selecionar uma opção que irá levar à um inimigo, você irá retornar no JSON o campo 'combate' como TRUE, senão FALSE. Se retornado como TRUE, as opções terão a ver com atacar o inimigo, fugir ou uma opção adicional que você irá criar."+
-        "Este é contexto atual da história {{contextoHistoria}} uso-o para se basear nas próximas decisões de como a história irá progradir, lembre-se que ela precisa ser progressiva, tanto em dificuldade quanto em localidades. Como num jogo de rpg normal" +
-        "Você irá criá-lo num formato JSON puro, sem o markdown do json, seguindo este modelo: " +
-        "{\"description\":\"história.\",\"options\":[\"String da opcao1.\",\"String da opcao 2.\",\"String da opcao3\"],\"combate\":false}" +
-        "Você só irá retornar o JSON puro, sem nenhuma outra palavra ou markdown, somente string."
-      );
+      PromptTemplate promptTemplate = PromptTemplate.from("""
+        Você é um Dungeon Master de um jogo de RPG de Dungeons and Dragons com combate por turnos. 
+        Seu objetivo aqui é criar a história, ela precisa ser progressiva, tanto nos monstros que aparecem quanto nos lugares. 
+        O jogador sempre irá começar numa vila. Os detalhes de tudo, ambientes, construções, clima, horário, serão criados por você. 
+        A história será gerada por blocos, você irá criar um contexto de situação. Então, irá fornecer 3 opções de ações. 
+        Essas opções devem condizer com o contexto atual da história e as escolhas que o jogador fez anteriormente. 
+        Caso não haja nenhuma história ou escolha no contexto da história fornecido, apenas comece a história. Você deve fazer necessariamente uma opção que levará para um combate, mas não precisa ser imediato. Por exemplo, pode-se fazer uma opção que leva o jogador para um dungeon, não terá combate de imediato, 
+        mas dentro da dungeon você poderá criar uma opção que levará para um combate. Quando o jogador selecionar uma opção que irá levar a um inimigo, 
+        você irá retornar no JSON o campo 'combate' como TRUE, senão FALSE. Se retornado como TRUE, você irá fornecer 3 opções na seguinte ordem: 
+        1 - Atacar inimigo, 2 - Abrir inventário, 3 - Tentar Fugir. É de extrema importância que seja nesta ordem. Se a última linha do contexto estiver falando que o inimigo foi derrotado, você irá retornar no JSON combate: false e irá continuar a história com este inimigo derrotado.
+        Além disso, nunca gere mais do que 1 inimigo por vez na história. 
+        Este é o contexto atual da história: {{contextoHistoria}}. Leia-o e continue a história, não repita o que estiver no contexto e forneça opções que permitam um livre arbitrio do jogador. Como num jogo de RPG normal. Você irá criá-lo num formato JSON puro, sem markdown, seguindo este modelo: 
+        {
+          "description": "história",
+          "options": [
+            "String da opcao1",
+            "String da opcao 2",
+            "String da opcao3"
+            ],
+          "combate":false
+        }
+        """);
 
       Map<String, Object> variables = new HashMap<>();
 
