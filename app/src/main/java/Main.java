8b51ceddec2cf2821887731a4d5de5d78777489a
victor-schneider <victor.8121966@edu.univali.br>
2025-05-07 12:50:26 -0300

    Refatorado componentes de gameplay e criado a possibilidade de abrir inventário e tentar fugir num combate

--- app/src/main/java/Main.java
+++ app/src/main/java/Main.java
@@ -29,6 +29,8 @@ import Utils.NumberGenerator;
 
 import Components.Enemy;
 import Components.PlayerComponents.Armor.Slot;
+import Gameplay.Combat;
+import Gameplay.Inventory;
 import Components.PlayerComponents.*;
 import Components.Context;
 
@@ -40,10 +42,8 @@ public class Main {
 
         /*
          * TODO
-         * 1 - Botar tudo para duas casas decimais - formatar duas casas
-         * 2 - Possibilidade de fugir de um combate
          * 3 - Fazer a IA gerar item ao jogador
-         * 4 - Croar JSON para aceitar items caso a IA retorne eles
+         * 4 - Criar JSON para aceitar items caso a IA retorne eles
          * 5 - Adicionar lógia de entrada de items ao inventário
          */
 
@@ -57,7 +57,7 @@ public class Main {
         PlayerSchema.initPlayer();
         ContextSchema.initContextDb();
 
-        Player player = new Player("Victor", 100, 5, 2, 5, 5, 1, 0, 20, 1);
+        Player player = new Player("Victor", 100, 5, 2, 90, 5, 1, 0, 20, 1);
         PlayerRepo.createPlayer(player);
 
         Armor helmet = new Armor("Capcete de ferro", Slot.HELMET, 10, 0, true, "ARMOR");
@@ -80,7 +80,7 @@ public class Main {
         int resposta = 0;
 
         int crit;
-        float atk = player.getAttack();
+        
         int[] genNumbers = new int[100];
         for (int i = 0; i < genNumbers.length; i++) {
             genNumbers[i] = -1;
@@ -112,116 +112,10 @@ public class Main {
                     respostaValida = true;
                 }
 
+                // CHAMA O INVENTÁRIO
                 if (resposta == 4) {
-                    int id = -1, respostaT = 0;                    
-
-                    while (respostaT != 3) {
-                        ClearConsole.clearConsole();
-                        System.out.println("===== INVENTÁRIO =====");
-                        PlayerInvRepo.listItems();
-                        System.out.println("[1] - Equipar um item");
-                        System.out.println("[2] - Desequipar um item");
-                        System.out.println("[3] - Sair do inventario");
-                        respostaT = scanner.nextInt();
-                        switch (respostaT) {
-                            case 1:
-                                    System.out.println("\n[0] - Sair do inventario \nSelecione o ID do item a ser equipado: ");
-                                    id = scanner.nextInt();
-                                    
-                                    if (id == 0) break;
-                                    id = Verifiers.itemIdVerifier(id);
-                                    if (id == 0) break;
-                                    
-                                    Item item = PlayerInvRepo.getItem(id);
-
-                                    if (item.getType().equals("WEAPON")) {
-                                        Weapon weapon = PlayerInvRepo.getWeapon(id);
-
-                                        while (id != 0 && weapon.getEquipped()) {
-                                            System.out.println("A arma ja esta equipada, digite outro ID ou 0 para sair");
-                                            id = scanner.nextInt();
-
-                                            if(id == 0) break;
-                                            id = Verifiers.weaponIdVerifier(id);
-                                            if(id == 0) break;
-                                            weapon = PlayerInvRepo.getWeapon(id);
-                                        };
-
-                                        player.equipWeapon(weapon);
-                                    } else if(item.getType().equals("ARMOR")) {
-                                        Armor armor = PlayerInvRepo.getArmor(id);
-
-                                        while (id != 0 && armor.getEquipped()) {
-                                            System.out.println("A armadura já está equipada, digite outro ID ou 0 para sair");
-                                            id = scanner.nextInt();
-
-                                            if(id == 0) break;
-                                            id = Verifiers.armorIdVerifier(id);
-                                            if(id == 0) break;
-
-                                            armor = PlayerInvRepo.getArmor(id);
-                                        }
-
-                                        player.equipArmor(armor);
-                                    }
-                                break;
-
-                            case 2:
-                            ClearConsole.clearConsole();
-                            System.out.println("===== ITEMS EQUIPADOS =====");
-                                PlayerInvRepo.listEquippedItems();
-                                System.out.println("\n[0] - Sair do inventario \nSelecione o ID do item a ser desequipado: ");
-                                id = scanner.nextInt();
-
-                                if (id == 0) break; 
-                                id = Verifiers.itemIdVerifier(id);
-                                if (id == 0) break;
-
-                                item = PlayerInvRepo.getItem(id);
-
-                                if (item.getType().equals("WEAPON")) {
-                                    Weapon weapon = PlayerInvRepo.getWeapon(id);
-
-                                    while( !weapon.getEquipped() && id != 0) {
-                                        System.out.println("A arma já está desequipada. Digite outro ID ou 0 para sair.");
-                                        id = scanner.nextInt();
-                                        
-                                        if(id == 0) break;
-                                        id = Verifiers.weaponIdVerifier(id);
-                                        if(id == 0) break;
-
-                                        weapon = PlayerInvRepo.getWeapon(id);
-                                    }
-                                    if(id == 0) break;
-
-                                    player.unequipWeapon(weapon);
-
-                                } else {
-                                    Armor armor = PlayerInvRepo.getArmor(id);
-
-                                    while( !armor.getEquipped() && id != 0) {
-                                        System.out.println("A armadura já está desequipada. Digite outro ID ou 0 para sair.");
-                                        id = scanner.nextInt();
-
-                                        if(id == 0) break;
-                                        id = Verifiers.armorIdVerifier(id);
-                                        if(id == 0) break;
-
-                                        armor = PlayerInvRepo.getArmor(id);
-                                    }
-                                    if(id == 0) break;
-
-                                    player.unequipArmor(armor);
-                                }
-                                break;
-
-                            case 3:
-                                break;
-                        }
-
-                    }
+                    Inventory.main(player);
                 }
-                    
 
             }
 
@@ -229,95 +123,24 @@ public class Main {
 
             if (contexto.getCombate() && resposta == 1) {
                 Enemy enemy = EnemyCreation.main(player, contexto.getDescription());
-                float enemyAtk = enemy.getAtk();
+                finalizar = Combat.main(contexto, player, enemy, genNumbers, finalizar);
 
-                System.out.println("Combate!\n\n");
-                enemy.getEnemyStatus(enemy);
+            } else if (contexto.getCombate() && resposta == 2) {
+                Inventory.main(player);
 
-                while (contexto.getCombate()) {
-                    int opcao = 0;
-                    int rng = NumberGenerator.main(100);
+            } else if (contexto.getCombate() && resposta == 3) {
+                Enemy enemy = EnemyCreation.main(player, contexto.getDescription());
+                int rng = NumberGenerator.main(100);
+                if(NumberGenerator.numberVerifier(rng, genNumbers, player.getDex())) {
+                    System.out.println(player.getName() + " Conseguiu fugir com sucesso!");
+                    ContextRepo.createContext(player.getName() + " Conseguiu fugir com sucesso do inimigo: " + enemy.getName());
+                    finalizar = false;
 
-                    for (int i = 0; i < genNumbers.length; i++) {
-                        genNumbers[i] = -1;
-                    }
+                } else {
+                    System.out.println(player.getName() + " Falhou em fugir!");
+                    finalizar = Combat.main(contexto, player, enemy, genNumbers, finalizar);
 
-                    System.out.println("Você decide: \n[1] - Atacar");
-                    opcao = scanner.nextInt();
-
-                    if (opcao == 1) {
-                        atk = player.getAtk();
-                        if (NumberGenerator.numberVerifier(rng, genNumbers, player.getLck() + 10)) {
-                            atk *= 2;
-                            System.out.println("Dano critico!\n");
-                        }
-                        if (atk == player.getAtk()) {
-                            atk -= ((atk * enemy.getDef()) / 100);
-                            atk = Verifiers.roundNumbers(atk);
-                        }
-
-                        if (NumberGenerator.numberVerifier(rng, genNumbers, enemy.getDex())) {
-                            System.out.println("O inimigo desviou do ataque!");
-                        } else {
-                            enemy.setHp( Verifiers.roundNumbers( enemy.getHp() - atk ) );
-                            System.out.println(player.getName() + " Infligiu " + atk + " de dano ao " + enemy.getName());
-                            EnemyRepo.updateEnemy(enemy);
-                            System.out.println(enemy.getName() + " HP: " + enemy.getHp());
-                        }
-                    }
-
-                    if (enemyAtk == enemy.getAtk()) {
-                        enemyAtk -= ((enemyAtk * player.getTotalDefense()) / 100);
-                        enemyAtk = Verifiers.roundNumbers(enemyAtk);
-                    }
-                    for (int i = 0; i < genNumbers.length; i++) {
-                        genNumbers[i] = -1;
-                    }
-
-                    if (NumberGenerator.numberVerifier(rng, genNumbers, player.getDex())) {
-                        System.out.println("O jogador desviou do ataque!");
-
-                    } else if (enemy.getHp() <= 0) {
-                        System.out.println("O inimigo foi derrotado! Jogador ganhou: " + enemy.getExp() + " de XP\n");
-                        player.setExp(enemy.getExp());
-                        if (player.getExp() > player.getLevel()) {
-                            player.setLevel(player.getLevel() + 1);
-                            player.setExp(0);
-                            System.out.println(player.getName() + " Subiu para o nível: " + player.getLevel() + "!\n");
-
-                            player.setNextLevel(player.getNextLevel() * 1.2);
-
-                            PlayerRepo.updatePlayer(player);
-                        }
-
-                        ContextRepo.createContext("Inimigo: " + enemy.getName()
-                                + " Foi derrotado, combate volta a ser FALSE e continue a historia contando como o inimigo morreu e qual é a próxima decisão do jogador");
-                        contexto = ContextCreation.main();
-                        contexto.setCombate(false);
-                        break;
-                    } else {
-                        System.out.println("O " + enemy.getName() + "  infligiu " + enemyAtk + " de dano ao jogador");
-                        player.setHp( Verifiers.roundNumbers( player.getHp() - enemyAtk) );
-                        PlayerRepo.updatePlayer(player);
-                    }
-
-                    if (player.getHp() <= 0) {
-                        System.out.println("Voce perdeu, ruim demais");
-                        contexto.setCombate(false);
-                        ContextRepo.createContext("Jogador: " + player.getName()
-                                + " Foi derrotado, finalize a história demonstrando como o jogador morreu para o inimigo: "
-                                + enemy.getName() + " que possuia a arma: " + enemy.getWeapon());
-
-                        contexto = ContextCreation.main();
-
-                        System.out.println(contexto.getDescription());
-
-                        finalizar = true;
-                        scanner.close();
-                        return;
-                    }
-                    System.out.println(player.getName() + "HP: " + player.getHp());
-                }
+                };
             }
 
             if (resposta == 5) {
